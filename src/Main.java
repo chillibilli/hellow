103809b40dba994adc4b3ed51f0944d698e486d7
chillibilli <y.dubrov@gmail.com>
2015-10-08 16:36:32 +0300

    closes #1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import sun.awt.windows.WToolkit;
+
 import javax.swing.*;
 import java.awt.*;
 
 /**
- * Created by y.dubrov on 07.10.2015.
+ *
  */
 public class Main {
 
@@ -12,10 +14,16 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Main");
         frame.setContentPane(new Main().p);
-        frame.getContentPane().setPreferredSize(new Dimension(300, 200));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setPreferredSize(new Dimension(DEF_WIDTH, DEF_HEIGHT));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
-        frame.setLocationByPlatform(true);
+        //frame.setLocationByPlatform(true);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = toolkit.getScreenSize();
+        frame.setLocation((screenSize.width - DEF_WIDTH) / 2, (screenSize.height - DEF_HEIGHT) / 2);
         frame.setVisible(true);
     }
+
+    final static int DEF_WIDTH = 300;
+    final static int DEF_HEIGHT = 200;
 }
